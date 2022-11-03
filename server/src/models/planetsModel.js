2ad1a1d98fa5eb061f16b8929ed60eadbb4ac5e3
Lê Văn Cường <vancuongle65@gmail.com>
2022-11-03 16:06:20 +0700

    up

---
+++ server/src/models/planetsModel.js
@@ -0,0 +1,48 @@
+const { rejects } = require('assert');
+const path = require('path');
+const { parse } = require('csv-parse');
+const fs = require('fs');
+const { resolve } = require('path');
+
+const habitablePlanets = [];
+
+function isHabitablePlanet(planet) {
+  return planet['koi_disposition'] === 'CONFIRMED'
+    && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
+    && planet['koi_prad'] < 1.6;
+}
+
+
+function loadPlanetsData(){
+   return new Promise((resolve, rejects) =>{
+        fs.createReadStream(path.join(__dirname,'..','..','data','kepler_data.csv'))
+        .pipe(parse({
+        comment: '#',
+        columns: true,
+        }))
+        .on('data', (data) => {
+        if (isHabitablePlanet(data)) {
+            habitablePlanets.push(data);
+        }
+        })
+        .on('error', (err) => {
+        console.log(err);
+        rejects(err);
+        })
+        .on('end', () => {
+        console.log(habitablePlanets.map((planet) => {
+            return planet['kepler_name'];
+        }));
+        console.log(`${habitablePlanets.length} habitable planets found!`);
+        resolve();
+    });
+   });
+}
+
+function getAllPlanets(){
+    return habitablePlanets;
+}
+  module.exports = {
+    loadPlanetsData,
+    getAllPlanets,
+  };
